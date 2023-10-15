623a12d0cbd1d27a75e54a4efc520950ba12a257
bkness <kbrandon863@gmail.com>
2023-10-15 14:03:30 -0700

    changed target swipe to the container so no unecpected bugs happen

--- assets/script.js
+++ assets/script.js
@@ -1,13 +1,12 @@
-// attempting to created swipe functionality for page navigation 
-let touchstartX = 0;
-let touchendX = 0;
 
-document.addEventListener('touchstart', function (event) {
+
+document.querySelector('.container').addEventListener('touchstart', function(event) {
     touchstartX = event.changedTouches[0].screenX;
 }, false);
 
-document.addEventListener('touchend', function (event) {
+document.querySelector('.container').addEventListener('touchend', function(event) {
     touchendX = event.changedTouches[0].screenX;
+    handleSwipe();
 }, false);
 
 function handleSwipe() {
